189372c37f888487d3c4a3e04763a9b286aa5329
MING-CHING WU <hangie921@gmail.com>
2016-01-29 23:17:26 +0800

    Update the index section and slotMachine effect and move to section 3 By Walter,20160129

--- JumpStart0122/FrontEnd/static/js/index.js
+++ JumpStart0122/FrontEnd/static/js/index.js
@@ -8,17 +8,26 @@ $(document).ready(function() {
 
     var machine2 = $('#slot2').slotMachine({
     	active : 1,
-    	delay : 500
+    	delay : 1000
     });
 
-    $("#slotButton").click(function(){
-		$('.slot:nth-child(3)').css({'visibility':'visible'});
+    var machine3 = $('#slot3').slotMachine({
+    	active : 1,
+    	delay : 1000
+    });
+
+    $("#slotButton2").click(function(){
+		$('#slot2 .slot:nth-child(3)').css({'visibility':'visible'});
 		machine2.shuffle(5,function(){
 			// location.href = '/contact_us';
 		});
+	});
 
-
-		
+	$("#slotButton3").click(function(){
+		$('#slot3 .slot:nth-child(3)').css({'visibility':'visible'});
+		machine3.shuffle(5,function(){
+			$.fn.fullpage.moveTo(3, 0);
+		});
 	});
 
 });

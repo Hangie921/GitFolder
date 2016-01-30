c3c118f2276bad995f45fc552d728fd949827468
MING-CHING WU <hangie921@gmail.com>
2016-01-30 11:13:44 +0800

    Update the section with slide

--- JumpStart0122/FrontEnd/static/js/index.js
+++ JumpStart0122/FrontEnd/static/js/index.js
@@ -6,28 +6,16 @@ $(document).ready(function() {
 
     });
 
-    var machine2 = $('#slot2').slotMachine({
-    	active : 1,
-    	delay : 1000
-    });
-
     var machine3 = $('#slot3').slotMachine({
     	active : 1,
-    	delay : 1000
+    	delay : 6000,
+    	auto : true,
+    	spins : true
     });
 
-    $("#slotButton2").click(function(){
-		$('#slot2 .slot:nth-child(3)').css({'visibility':'visible'});
-		machine2.shuffle(5,function(){
-			// location.href = '/contact_us';
-		});
-	});
 
 	$("#slotButton3").click(function(){
-		$('#slot3 .slot:nth-child(3)').css({'visibility':'visible'});
-		machine3.shuffle(5,function(){
-			$.fn.fullpage.moveTo(3, 0);
-		});
+		machine3.stop();
 	});
 
 });

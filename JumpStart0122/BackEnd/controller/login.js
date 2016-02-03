75c29bcc9bb1259bf39634a25c60269bce9c50dc
MING-CHING WU <hangie921@gmail.com>
2016-02-03 09:56:54 +0800

    sync

--- JumpStart0122/BackEnd/controller/login.js
+++ JumpStart0122/BackEnd/controller/login.js
@@ -2,7 +2,7 @@ var login = require('../module/login');
 
 function route(app, mongoClient){
 	app.post('/login',function(req,res){  
-		login.check()
+		login.check();
 		//call the login module
 		res.render('index');
 	});

5270b1586e9148411a738084ac8d89475f748c45
MING-CHING WU <hangie921@gmail.com>
2016-02-26 13:51:11 +0800

    fix the problem that when you press the tab btn will jump the next slide

--- JumpStart0122/FrontEnd/static/js/jquery.validate.js
+++ JumpStart0122/FrontEnd/static/js/jquery.validate.js
@@ -330,7 +330,7 @@ $.extend( $.validator, {
 		url: "Please enter a valid URL.",
 		date: "Please enter a valid date.",
 		dateISO: "Please enter a valid date ( ISO ).",
-		number: "Please enter a valid number.",
+		number: "請輸入有效數字",
 		digits: "Please enter only digits.",
 		creditcard: "Please enter a valid credit card number.",
 		equalTo: "Please enter the same value again.",
